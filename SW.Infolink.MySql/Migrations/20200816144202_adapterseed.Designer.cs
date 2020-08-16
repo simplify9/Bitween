61fda675a2aa44758a3001250815c17bee0dbd26
Samer Awajan <samer@simplify9.com>
2020-08-16 17:42:32 +0300

    migration addition

--- SW.Infolink.MySql/Migrations/20200816144202_adapterseed.Designer.cs
+++ SW.Infolink.MySql/Migrations/20200816144202_adapterseed.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SW.Infolink;
 
 namespace SW.Infolink.MySql.Migrations
 {
     [DbContext(typeof(InfolinkDbContext))]
-    partial class InfolinkDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200816144202_adapterseed")]
+    partial class adapterseed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
