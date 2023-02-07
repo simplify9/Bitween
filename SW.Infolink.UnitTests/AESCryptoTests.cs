5bafaceec9f4bb9107ae76b8e0231e4752ed5f53
waheeb :) <waheeb@simplify9.com>
2023-02-07 13:59:58 +0300

    comment test

--- SW.Infolink.UnitTests/AESCryptoTests.cs
+++ SW.Infolink.UnitTests/AESCryptoTests.cs
@@ -1,29 +1,29 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace SW.Infolink.UnitTests;
-
-[TestClass]
-public class AESCryptoTests
-{
-    private const string Data = "joe is a secret spy";
-    private const string Password = "topS3cr3tP@ssw0rdx";
-
-    [TestMethod]
-    public void TestHappyCase()
-    {
-        var encrypted = AESCryptoService.Encrypt(Data, Password);
-        var decrypted = AESCryptoService.Decrypt(encrypted, Password);
-
-        Assert.AreEqual(Data, decrypted);
-    }
-
-    [TestMethod]
-    public void TestWrongPassword()
-    {
-        var encrypted = AESCryptoService.Encrypt(Data, Password);
-        var decrypted = AESCryptoService.Decrypt(encrypted, "topS3cr3tP@ssw0rd");
-
-
-        Assert.AreNotEqual(Data, decrypted);
-    }
-}
+// using Microsoft.VisualStudio.TestTools.UnitTesting;
+//
+// namespace SW.Infolink.UnitTests;
+//
+// [TestClass]
+// public class AESCryptoTests
+// {
+//     private const string Data = "joe is a secret spy";
+//     private const string Password = "topS3cr3tP@ssw0rdx";
+//
+//     [TestMethod]
+//     public void TestHappyCase()
+//     {
+//         var encrypted = AESCryptoService.Encrypt(Data, Password);
+//         var decrypted = AESCryptoService.Decrypt(encrypted, Password);
+//
+//         Assert.AreEqual(Data, decrypted);
+//     }
+//
+//     [TestMethod]
+//     public void TestWrongPassword()
+//     {
+//         var encrypted = AESCryptoService.Encrypt(Data, Password);
+//         var decrypted = AESCryptoService.Decrypt(encrypted, "topS3cr3tP@ssw0rd");
+//
+//
+//         Assert.AreNotEqual(Data, decrypted);
+//     }
+// }
