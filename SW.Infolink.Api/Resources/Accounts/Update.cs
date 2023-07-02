488f9853d84ade1d84fd74dfaa905da56b9bd707
waheeb :) <waheeb@simplify9.com>
2023-07-02 13:28:59 +0300

    update account

---
+++ SW.Infolink.Api/Resources/Accounts/Update.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Threading.Tasks;
+using SW.Infolink.Domain.Accounts;
+using SW.Infolink.Model;
+using SW.PrimitiveTypes;
+
+namespace SW.Infolink.Resources.Accounts;
+
+public class Update : ICommandHandler<int, UpdateAccountModel>
+{
+    private readonly InfolinkDbContext _dbContext;
+    private readonly RequestContext _requestContext;
+
+    public Update(InfolinkDbContext dbContext, RequestContext requestContext)
+    {
+        _dbContext = dbContext;
+        _requestContext = requestContext;
+    }
+
+    public async Task<object> Handle(int key, UpdateAccountModel request)
+    {
+        var loggedInUserId = Convert.ToInt32(_requestContext.GetNameIdentifier());
+
+        if (key != loggedInUserId)
+            _requestContext.EnsureAccess(AccountRole.Admin);
+
+
+        var accountId = Convert.ToInt32(_requestContext.GetNameIdentifier());
+        var account = await _dbContext.Set<Account>().FindAsync(accountId);
+        if (account is null)
+            throw new SWValidationException("ACCOUNT_NOT_FOUND", $"No account exists with the id {accountId}");
+
+
+        account.Update(request.Name, (AccountRole)request.Role);
+        await _dbContext.SaveChangesAsync();
+
+        return null;
+    }
+}
